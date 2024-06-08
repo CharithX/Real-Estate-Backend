a23e5bef666560974944cf8d908f2df808aac6ed
CharithX <it20606374@my.sliit.lk>
2024-06-09 01:51:58 +0530

    updated db and set up cors for client communicate

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-import express from "express"
-import authRoute from "./routes/auth.route.js"
-
+import express from "express";
+import authRoute from "./routes/auth.route.js";
+import { MongoClient } from "mongodb";
+import cookieParser from "cookie-parser";
+import cors from "cors"
 const app = express();
 
+app.use(express.json());
 
-app.use(express.json())
+
+app.use(cors({ origin: process.env.CLIENT_URL , credentials:true}));
+
+app.use(cookieParser())
 app.use("/api/test", (req, res) => {
-    res.send("test worked")
-})
+  res.send("API is working");
+});
 
-app.use("/api/auth", authRoute)
+app.use("/api/auth", authRoute);
 
+const url = process.env.DATABASE_URL;
 
-  app.listen(8800, () => {
-    console.log("server is running on port 8800!");
-})
+// Function to connect to MongoDB
+async function connectToMongoDB() {
+  const client = new MongoClient(url);
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } finally {
+    // Close the connection
+    await client.close();
+  } 
+}
+
+const port = process.env.PORT || 8800;
+
+// Start the server and connect to MongoDB
+app.listen(port, async () => {
+  console.log(`server is running on port ${port}`);
+  await connectToMongoDB();
+});
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,76 @@
-export const register = (req, res) => {
-    const {username, email, password} = req.body
-    console.log(req.body)
-}
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
 
-export const login = (req, res) => {
-  //db
+
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  console.log(req.body);
+
+  try {
+    //hash the password
+
+    const hashedpassword = await bcrypt.hash(password, 10);
+    console.log(hashedpassword);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedpassword,
+      },
+    });
+
+    console.log(newUser);
+    res.status(201).json({ message: "User created Succesfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "User created Failed" });
+  }
 };
 
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    //check if user exist
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
+    //check if password correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    //generate cookie token and send to user
+    // res.setHeader("set-Cookie", "test=" + "myValue").json({ message: "succesfully logged in" })
+    const age = 1000 * 60 * 60 * 24 * 7;
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_TOKEN,
+      {expiresIn: age}
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: age,
+      })
+      .status(200)
+      .json({ message: "login succesfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "login failed" });
+  }
+};
 
 export const logout = (req, res) => {
-  //db
+
+res.clearCookie("token").status(200).json({message: "logout succeslly"})
+
 };
