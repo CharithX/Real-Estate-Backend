507275d997e70426291fd710ebbad2417df7cee0
CharithX <it20606374@my.sliit.lk>
2024-07-03 09:57:53 +0530

    api update

--- app.js
+++ app.js
@@ -4,25 +4,15 @@ import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import { MongoClient } from "mongodb";
-import cookieParser from "cookie-parser"; 
-import cors from "cors"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
-app.use(cors({ origin: process.env.CLIENT_URL , credentials:true}));
-
-app.use(cookieParser())
-app.use("/", (req, res) => {
-  res.send("API is working");
-});
-
-app.use("/api/auth", authRoute);
-app.use("/api/test", testRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-
+app.use(cookieParser());
 const url = process.env.DATABASE_URL;
 
 // Function to connect to MongoDB
@@ -35,11 +25,18 @@ async function connectToMongoDB() {
   } finally {
     // Close the connection
     await client.close();
-  } 
+  }
 }
 
-const port = process.env.PORT || 8800;
+app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
+const port = process.env.PORT || 8800;
+app.use("/", (req, res) => {
+  res.send("API is working");
+});
 // Start the server and connect to MongoDB
 app.listen(port, async () => {
   console.log(`server is running on port ${port}`);
